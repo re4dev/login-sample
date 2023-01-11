78428ab88bd50fc421d3f23ed120f30ec52384cc
zareex <63774713+zareex@users.noreply.github.com>
2023-01-11 19:26:15 +0100

    feat: :sparkles: [WIP] Add first login

--- pages/Components/Login/Login.tsx
+++ pages/Components/Login/Login.tsx
@@ -4,32 +4,60 @@ import { shallow } from 'zustand/shallow'
 import { useTokenStore } from '../../../store'
 
 const Login = () => {
-  const {token, userId } = useTokenStore(
-    (state) => ({token: state.token, userId: state.userId}),
+  const { token, userId } = useTokenStore(
+    (state) => ({ token: state.token, userId: state.userId }),
     shallow
   )
   const setToken = useTokenStore((s) => s.setToken);
   const clearToken = useTokenStore((s) => s.clearToken);
   const isLoggedIn = useTokenStore((s) => s.isLoggedIn);
-  const mailRef = useRef<HTMLInputElement>(null);
+  const userData = {
+    userName: useRef<HTMLInputElement>(null),
+    userPassword: useRef<HTMLInputElement>(null)
+  }
+
+
+  
+  const url: string = process.env.NEXT_PUBLIC_API_LINK_FOR_LOGIN;
+
+  function login() {
+    console.log(userData.userName.current.value)
+    fetch(url,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          userName: userData.userName.current.value,
+          userPassword: userData.userPassword.current.value
+        })
+      })
+      .then((response) => response.json())
+      .then((data) => { 
+        console.log(data)
+        setToken(data.token)
+    })
+  }
+
 
   return (
     <Grid.Container justify="center">
       <Grid xs={4}>
         <Card>
-          <Card.Body css={{ padding: "$15"}}>
+          <Card.Body css={{ padding: "$15" }}>
             <Text h2>Login</Text>
             <Spacer y={0.5} />
             <form>
-                <Input labelPlaceholder="E-Mail" css={{ w: "250px"}} ref={mailRef}/>
-                <Spacer y={1.3} />
-                <Input.Password labelPlaceholder="Password" />
-                <Spacer y={1} />
-                {isLoggedIn()
-                  ? <Button shadow color="error" auto css={{ w: "150px"}} onClick={() => clearToken()}>Logout</Button>
-                  : <Button shadow color="primary" auto css={{ w: "150px"}} onClick={() => setToken(mailRef.current?.value)}>Login</Button>
-                }
-                <Text>{token}</Text>
+              <Input labelPlaceholder="Username" css={{ w: "250px" }} ref={userData.userName} />
+              <Spacer y={1.3} />
+              <Input.Password labelPlaceholder="Password" ref={userData.userPassword}/>
+              <Spacer y={1} />
+              {isLoggedIn()
+                ? <Button shadow color="error" auto css={{ w: "150px" }} onClick={() => clearToken()}>Logout</Button>
+                : <Button shadow color="primary" auto css={{ w: "150px" }} onClick={() => login()}>Login</Button>
+              }
+              <Text>{token}</Text>
             </form>
           </Card.Body>
         </Card>
