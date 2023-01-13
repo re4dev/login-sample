f2844f5febeba6a5dff6d2d651cb4f7efa0e3c60
zareex <63774713+zareex@users.noreply.github.com>
2023-01-13 18:35:06 +0100

    feat: :lipstick: Add error message and change UI when logged in

--- pages/Components/Login/Login.tsx
+++ pages/Components/Login/Login.tsx
@@ -1,20 +1,27 @@
 import { Button, Card, Grid, Input, Spacer, Text } from '@nextui-org/react'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { shallow } from 'zustand/shallow'
 import { useTokenStore } from '../../../store'
 
 const Login = () => {
-  const { token, userId } = useTokenStore(
-    (state) => ({ token: state.token, userId: state.userId }),
+  const { userId, isLoggedIn, setToken, setUserId, clearToken } = useTokenStore(
+    (state) => (
+      {
+        token: state.token, 
+        userId: state.userId, 
+        isLoggedIn: state.isLoggedIn,
+        setToken: state.setToken,
+        setUserId: state.setUserId,
+        clearToken: state.clearToken
+      }
+    ),
     shallow
   )
-  const setToken = useTokenStore((s) => s.setToken);
-  const clearToken = useTokenStore((s) => s.clearToken);
-  const isLoggedIn = useTokenStore((s) => s.isLoggedIn);
   const userData = {
     userName: useRef<HTMLInputElement>(null),
     userPassword: useRef<HTMLInputElement>(null)
   }
+  const [message, setMessage] = useState<string>("");
 
 
   
@@ -37,7 +44,8 @@ const Login = () => {
       .then((data) => { 
         console.log(data)
         setToken(data.token)
-    })
+        setUserId(data.userId)
+        setMessage(error.message);
   }
 
 
@@ -46,19 +54,15 @@ const Login = () => {
       <Grid xs={4}>
         <Card>
           <Card.Body css={{ padding: "$15" }}>
-            <Text h2>Login</Text>
-            <Spacer y={0.5} />
-            <form>
-              <Input labelPlaceholder="Username" css={{ w: "250px" }} ref={userData.userName} />
-              <Spacer y={1.3} />
-              <Input.Password labelPlaceholder="Password" css={{ w: "250px" }} ref={userData.userPassword}/>
-              <Spacer y={1} />
-              {isLoggedIn()
-                ? <Button shadow color="error" auto css={{ w: "150px" }} onClick={() => clearToken()}>Logout</Button>
-                : <Button shadow color="primary" auto css={{ w: "150px" }} onClick={() => login()}>Login</Button>
-              }
-              <Text>{token}</Text>
-            </form>
+            {isLoggedIn()
+              ?<><Text h2 color='red'>Hello {userData.userName.current.value}</Text><Spacer y={0.3} /><Text>User-ID: {userId}</Text><Spacer y={0.3} /> </>
+              : <><Text h2>Login</Text><Spacer y={0.7} /><Input labelPlaceholder="Username" css={{ w: "250px" }} ref={userData.userName} /><Spacer y={1.3} /><Input.Password labelPlaceholder="Password" css={{ w: "250px" }} ref={userData.userPassword} /><Spacer y={1} /></>
+            }
+            {isLoggedIn()
+              ? <Button shadow color="error" auto css={{ w: "150px" }} onClick={() => { clearToken(); setMessage(""); }}>Logout</Button>
+              : <Button shadow color="primary" auto css={{ w: "150px" }} onClick={() => login()}>Login</Button>
+            }
+            {message && !isLoggedIn() ? <Text>{message}</Text> : null}
           </Card.Body>
         </Card>
       </Grid>
