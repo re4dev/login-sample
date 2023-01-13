305017bacaba642c7ca9d7fb5336e16a138731ed
zareex <63774713+zareex@users.noreply.github.com>
2023-01-13 18:26:16 +0100

    feat: :sparkles: Handle userId with zustand

--- store.ts
+++ store.ts
@@ -5,13 +5,17 @@ type tokenStore = {
     userId?: number,
     setToken: (token?: string) => void,
     clearToken: () => void,
+    setUserId: (userId: number) => void,
+    clearUserId: () => void,
     isLoggedIn: () => boolean
 };
 
 export const useTokenStore = create<tokenStore>((set, get) => ({
     token: "",
-    userId: 100,
+    userId: null,
     setToken: (token) => set({token}),
     clearToken: () => set({token: ""}),
+    setUserId: (userId) => set({userId}),
+    clearUserId: () => set({userId: null}),
     isLoggedIn: () => get().token != "" ? true : false
 }))
