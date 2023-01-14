e6cbe4ddb39880bce92bd84afc3ed64c99d6df41
zareex <63774713+zareex@users.noreply.github.com>
2023-01-14 16:29:19 +0100

    refactor: :bug: Fix problem that the form cannot be submitted with enter

--- pages/Components/Login/Login.tsx
+++ pages/Components/Login/Login.tsx
@@ -27,7 +27,9 @@ const Login = () => {
 
   const url: string = process.env.NEXT_PUBLIC_API_LINK_FOR_LOGIN;
 
-  function login() {
+  function login(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
     fetch("/api/login",
       {
         method: "POST",
@@ -57,12 +59,8 @@ const Login = () => {
         <Card>
           <Card.Body css={{ padding: "$15" }}>
             {isLoggedIn()
-              ?<><Text h2 color='red'>Hello {userData.userName.current.value}</Text><Spacer y={0.3} /><Text>User-ID: {userId}</Text><Spacer y={0.3} /> </>
-              : <><Text h2>Login</Text><Spacer y={0.7} /><Input labelPlaceholder="Username" css={{ w: "250px" }} ref={userData.userName} /><Spacer y={1.3} /><Input.Password labelPlaceholder="Password" css={{ w: "250px" }} ref={userData.userPassword} /><Spacer y={1} /></>
-            }
-            {isLoggedIn()
-              ? <Button shadow color="error" auto css={{ w: "150px" }} onClick={() => { clearToken(); setMessage(""); }}>Logout</Button>
-              : <Button shadow color="primary" auto css={{ w: "150px" }} onClick={() => login()}>Login</Button>
+              ?<><Text h2 color='red'>Hello {userData.userName.current.value}</Text><Spacer y={0.3} /><Text>User-ID: {userId}</Text><Spacer y={0.3} /><Button shadow color="error" auto css={{ w: "150px" }} onClick={() => { clearToken(); setMessage(""); }}>Logout</Button></>
+              : <><form onSubmit={login}><Text h2>Login</Text><Spacer y={0.7} /><Input labelPlaceholder="Username" css={{ w: "250px" }} ref={userData.userName} /><Spacer y={1.3} /><Input.Password labelPlaceholder="Password" css={{ w: "250px" }} ref={userData.userPassword} /><Spacer y={1} /><Button shadow color="primary" auto css={{ w: "150px" }} type="submit">Login</Button></form></>
             }
             {message && !isLoggedIn() ? <Text>{message}</Text> : null}
           </Card.Body>
